8d42a76d80f388fc470ec55ab6ef274f708d929e
JayPanoz <JayPanoz@users.noreply.github.com>
2025-12-18 16:57:05 +0000

    deploy: fdc9d2823e9ac7c67d09fd8ae709bfbdca0fe3b0

--- build/voices/packages.d.ts
+++ build/voices/packages.d.ts
@@ -1,8 +1,2 @@
 import { TQuality } from './types';
-export declare enum PackageQuality {
-    VeryLow = "super-compact",
-    Low = "compact",
-    Normal = "enhanced",
-    High = "premium"
-}
 export declare const getInferredQualityFromPackageName: (voiceName: string) => TQuality | undefined;
