e9f0d2694b230969d9f83bafcd7fbb617e971108
HadrienGardeur <HadrienGardeur@users.noreply.github.com>
2026-01-18 15:40:23 +0000

    deploy: a308973e3f80db6fdfeb80da56d79543b4d3b1c9

---
+++ build/kk-BdLCAb2s.js
@@ -0,0 +1,13 @@
+const t = "kk", a = "kk-KZ", e = "Сәлеметсіз бе, менің атым {name} және мен қазақ дауыстымын.", l = [{ label: "Aigul", name: "Microsoft Aigul Online (Natural) - Kazakh (Kazakhstan)", language: "kk-KZ", gender: "female", quality: ["veryHigh"], rate: 1, pitchControl: !1, browser: ["Edge"], preloaded: !0 }, { label: "Daulet", name: "Microsoft Daulet Online (Natural) - Kazakh (Kazakhstan)", language: "kk-KZ", gender: "male", quality: ["veryHigh"], rate: 1, pitchControl: !1, browser: ["Edge"], preloaded: !0 }, { label: "Aru", name: "Aru", nativeID: ["com.apple.voice.Aru"], localizedName: "apple", language: "kk-KZ", gender: "female", quality: ["normal"], rate: 1, pitch: 1, os: ["macOS", "iOS", "iPadOS"] }], n = {
+  language: "kk",
+  defaultRegion: a,
+  testUtterance: e,
+  voices: l
+};
+export {
+  n as default,
+  a as defaultRegion,
+  t as language,
+  e as testUtterance,
+  l as voices
+};
